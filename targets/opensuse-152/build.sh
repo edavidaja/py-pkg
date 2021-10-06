99d3666f891203157dbfd682907ebca81d7a9cad
E. David Aja <edavidaja@hey.com>
2021-10-06 03:23:40 -0500

    add opensuse support (#1)

---
+++ targets/opensuse-152/build.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -xeu
+echo "Build script starting..."
+
+cp /src/Python-${VERSION}.tgz /
+
+tar -zxvf Python-${VERSION}.tgz
+
+cd Python-${VERSION}
+
+./configure \
+    --prefix=/opt/python/${VERSION} \
+    --enable-shared \
+    --enable-ipv6 \
+    LDFLAGS=-Wl,-rpath=/opt/python/${VERSION}/lib,--disable-new-dtags
+
+make
+make install
+
+fpm \
+  -s dir \
+  -t deb \
+  -v ${BUILD_NUMBER} \
+  -n python${VERSION} \
+  -p /output/${OS_IDENTIFIER}/ \
+  /opt/python/${VERSION}
